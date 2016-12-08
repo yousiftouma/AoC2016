e66349f0dd271176df0888c370fe3e8e506638ae
Yousif Touma <youto814@student.liu.se>
2016-12-08 13:59:36 +0100

    add solution for day 4 part 1

---
+++ aoc4a.py
@@ -0,0 +1,35 @@
+
+
+def isOk(words, hsh):
+	for c in hsh:
+		if c not in words:
+			return False
+		val = words[c]
+		del words[c]
+		for elem, value in words.items():
+			if value > val:
+				return False
+			elif value == val:
+				if elem < c:
+					return False
+	return True
+
+res = 0
+while True:
+	words = {}
+	values = input().split("-")
+	id = -1
+	hsh = ""
+	for code in values:
+		if not "[" in code:
+			for chr in code:
+				if chr not in words:
+					words[chr] = 0
+				else:
+					words[chr] += 1
+		else:
+			id = int(code[0:code.find("[")])
+			hsh = code[code.find("[")+1:-1]
+	if isOk(words, hsh):
+		res += id
+	print("current res is " + str(res))
